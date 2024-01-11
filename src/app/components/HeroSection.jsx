93add75c2778e1bca30aa52a8c3fe51b86683314
Lucas Alves <80328704+LucasAlv3s@users.noreply.github.com>
2024-01-10 23:24:37 -0300

    Criando o HeroSection

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return <section>
+    <h1 className="text-white">Olá, Eu me chamo Lucas Alves</h1>
+  </section>
+}
+
+export default HeroSection
