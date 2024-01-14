e4c4c81c4c49b8c9a15ac308cf37ac34ed755015
Lucas Alves <80328704+LucasAlv3s@users.noreply.github.com>
2024-01-14 16:20:12 -0300

    Inserindo o pacote react-type-animation

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
     <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 via-blue-800 to-pink-600'>Hello, I'm {""}
+            <span className='text-transparent bg-clip-text bg-gradient-to-br from-green-400 to-blue-500'>Hello, I'm {""}
             </span>
             Lucas
           </h1>
@@ -14,8 +14,8 @@ const HeroSection = () => {
             E-commerce Analyst | Full-Stack Developer(Freelancer)
           </p>
           <div>
-            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-300 text-white'>Contrate-me</button>
-            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-blue-500 via-purple-500 to-pink-500 hover:bg-slate-800 text-white mt-3'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
+            <button className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white'>Contrate-me</button>
+            <button className='px-1 py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white mt-3'><span className='block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2'>Download CV</span></button>
           </div>
         </div>
         <div className='col-span-5 place-self-center mt-4 lg:mt-0'>
