a27ca58292cba6601e6f5c2ab4fa9c1e38125ee6
Lucas Alves <80328704+LucasAlv3s@users.noreply.github.com>
2024-01-28 12:32:42 -0300

    Inserindo algumas alterações de Links e Images

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-
+import Link from '@/node_modules/next/link';
+import Image from '@/node_modules/next/Image';
 
 export default function Home() {
   return (
@@ -11,8 +12,47 @@ export default function Home() {
               E-commerce Analyst and Full-Stack Developer
             </span>
           </h1>
+          <p className="text-gray-200 hidden md:block">
+            Lucas é um analista de e-commerce e desenvolvedor full-stack freelancer, com interesse em seguir carreira acadêmica como professor de tecnologia da informação.
+          </p>
+          <div className="flex-col md:flex-row hidden md:flex">
+            <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
+              Ler mais...
+            </Link>
+            <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              Meus projetos
+            </Link>
+            <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
+              <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
+              Contato
+            </Link>
+          </div>
         </div>
       </div>
+
+      <div className=" absolute bottom-10 z-[10] right-5  flex-col md:hidden">
+        <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
+          Ler mais...
+        </Link>
+        <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
+          Meus projetos
+        </Link>
+        <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
+          Contato
+        </Link>
+      </div>
+
+      <div className="absolute bottom-0 right-0 z-[10]">
+        <Image 
+          src={'/horse.png'} 
+          alt={'horse'}
+          height={300}
+          width={300}
+          className="absolute right-55 top-40"
+        />
+      </div>
+
     </main>
   );
 }
