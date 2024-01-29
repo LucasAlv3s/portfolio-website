8fb91cc0d9a1d0cbed6e23c599ac03069ee7e6c0
Lucas Alves <80328704+LucasAlv3s@users.noreply.github.com>
2024-01-29 13:07:57 -0300

    Inserindo meus serviços como Desenvolvedor Full-Stack Freelancer e Analista de E-commerce

--- app/page.tsx
+++ app/page.tsx
@@ -12,10 +12,15 @@ export default function Home() {
       >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
-            Make anything possible with
+            Lucas Alves
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
               {" "}
-              Web Development
+              <br/>
+              Full-Stack Developer
+              <br/>
+              and 
+              <br/>
+              E-commerce Analyst
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block">
