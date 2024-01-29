19461415732cf7417f4dc4e87db0344490acb165
Lucas Alves <80328704+LucasAlv3s@users.noreply.github.com>
2024-01-29 12:56:40 -0300

    Atualizações de botões e imagens na page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,103 @@
-import Link from '@/node_modules/next/link';
-import Image from '@/node_modules/next/Image';
+"use client"
+
+import Image from "@/node_modules/next/image";
+import Link from "@/node_modules/next/link";
 
 export default function Home() {
   return (
     <main className="w-screen h-screen relative">
-      <div className="flex items-center w-full h-full bg-cover bg-center" style={{backgroundImage: "url(/main-bg.webp)"}}>
+      <div
+        className="flex items-center w-full h-full bg-cover bg-center"
+        style={{ backgroundImage: "url(/main-bg.webp)" }}
+      >
         <div className="pl-20 md:pl-40 pb-56 md:pb-20 flex flex-col gap-5 z-[10] max-w-[750px]">
           <h1 className="text-[50px] text-white font-semibold">
-            Make anything possible with 
-            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
-              E-commerce Analyst and Full-Stack Developer
+            Make anything possible with
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-red-500">
+              {" "}
+              Web Development
             </span>
           </h1>
           <p className="text-gray-200 hidden md:block">
-            Lucas é um analista de e-commerce e desenvolvedor full-stack freelancer, com interesse em seguir carreira acadêmica como professor de tecnologia da informação.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
           </p>
-          <div className="flex-col md:flex-row hidden md:flex">
-            <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
-              Ler mais...
+          <div className="flex-col md:flex-row hidden md:flex gap-5">
+            <Link
+              href="/my-skills"
+              className="rounded-[20px] group relative bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]"
+            >
+              Learn more
             </Link>
-            <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
+            <Link
+              href="/my-projects"
+              className="rounded-[20px] group relative bg-trasparent px-5 border border-white py-3 text-lg text-white max-w-[200px]"
+            >
               <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
-              Meus projetos
+              My projects
             </Link>
-            <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
+            <Link
+              href="/contact-me"
+              className="rounded-[20px] group relative bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+            >
               <div className="absolute rounded-[20px] z-[1] bg-white inset-0 opacity-0 group-hver:opacity-20" />
-              Contato
+              Contact me
             </Link>
           </div>
         </div>
       </div>
 
-      <div className=" absolute bottom-10 z-[10] right-5  flex-col md:hidden">
-        <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
-          Ler mais...
+      <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
+        <Link
+          href="/my-skills"
+          className="rounded-[20px] group bg-blue-500 px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Learn more
         </Link>
-        <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
-          Meus projetos
+
+        <Link
+          href="/my-projects"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          My projects
         </Link>
-        <Link href="/my-skills" className="rounded-[20px] group bg-blue-500 hover:bg-blue-400 px-5 py-3 text-lg text-white max-w-[200px]">
-          Contato
+        <Link
+          href="/contact-me"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Contact me
         </Link>
       </div>
 
       <div className="absolute bottom-0 right-0 z-[10]">
-        <Image 
-          src={'/horse.png'} 
-          alt={'horse'}
+        <Image
+          src="/horse.png"
+          alt="horse"
           height={300}
           width={300}
           className="absolute right-55 top-40"
         />
+
+        <Image src="/cliff.webp" alt="cliff" width={480} height={480} />
       </div>
 
+      <div className="absolute bottom-0 z-[5] w-full h-auto">
+        <Image
+          src="/trees.webp"
+          alt="trees"
+          width={2000}
+          height={2000}
+          className="w-full h-full"
+        />
+      </div>
+
+      <Image
+        src="/stars.png"
+        alt="stars"
+        height={300}
+        width={300}
+        className="absolute top-0 left-0 z-[10]"
+      />
     </main>
   );
 }
